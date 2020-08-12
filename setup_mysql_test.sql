b07576293b358f3abe0757e8db2024c41ab89037
AndrewKalil <1541@holbertonschool.com>
2020-08-12 16:14:11 -0500

    pep8

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,10 @@
 -- create if not exit new database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db
 -- create the new user for database hbnb_dev
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
 -- granting priviledges
-GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost'
 -- have SELECT privilege on the database
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
 -- flush priviledges
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES
