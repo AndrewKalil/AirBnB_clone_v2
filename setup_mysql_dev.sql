813e90f5808bf919acf8deed4be7a2176355e927
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-12 23:04:12 -0500

    fixing problems ; in files sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 -- create if not exit new database hbnb_dev_db
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- create the new user for database hbnb_dev
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- granting priviledges
-GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON hbnb_dev_db. * TO 'hbnb_dev'@'localhost';
 -- have SELECT privilege on the database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 -- flush priviledges
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
