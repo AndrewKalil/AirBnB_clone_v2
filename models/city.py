748a45281a3eca901652871b102982f9962b154e
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-08-12 22:31:56 -0500

    Revert "Revert "Revert "Revert "fixing problems in the files city and state""""
    This reverts commit 02254470679855139742d17bfe053b8b6e9bbbb2.

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -22,5 +22,5 @@ class City(BaseModel, Base):
                       nullable=False)
 
     places = relationship("Place",
-                          backref="cities",
+                          backref=backref("cities", cascade='all'),
                           cascade="all, delete-orphan")
